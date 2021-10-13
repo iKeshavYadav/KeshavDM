047110c3d11760c0dcb844693e503af1970f81b1
A1278 <a1278@A1278s-MacBook-Pro.local>
2021-10-13 16:38:47 +0530

    initial commit

---
+++ mail.php
@@ -0,0 +1,31 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$category = $_POST["category"];
+$message = $_POST["message"];
+
+
+$EmailTo = "000.lunchbox@gmail.com";
+$Title = "New Message Received";
+
+// prepare email body text
+$Fields .= "Name: ";
+$Fields .= $name;
+$Fields .= "\n";
+
+$Fields .= "Email: ";
+$Fields .= $email;
+$Fields .= "\n";
+
+$Fields .= "Category: ";
+$Fields .= $category;
+$Fields .= "\n";
+
+$Fields .= "Message: ";
+$Fields .= $message;
+$Fields .= "\n";
+
+
+// send email
+$success = mail($EmailTo,  $Title,  $Fields, "From:".$email);
+
